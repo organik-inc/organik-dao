1e2de69122787e328f3a08cf28f3e44e3b75d616
organik-inc <luis@organik.express>
2022-02-05 16:33:53 +0000

    Added ";" on a few lines but new errors are thrown

--- contracts/OrganikDAO.sol
+++ contracts/OrganikDAO.sol
@@ -5,29 +5,29 @@
 /**/
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+// OpenZeppelin Contracts v4.4.1 (token/ERC20/ERC20.sol)
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.4/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-// OpenZeppelin Contracts v4.4.1 (token/ERC20/ERC20.sol)
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.4/contracts/token/ERC20/ERC20.sol"
 // Organik Contracts v0.0.1 (contracts/OrganikDAO.sol)
 contract OrganikDAO is ReentrancyGuard, AccessControl, ERC20 {
-
-    constructor () public ERC20 ("OrganikDAO", "ORGANIK") {
-        _mint(msg.sender, 1000000 * (10 ** uint256(decimals()) ) )
-        // This line will mint 1M coins to the Contract Address.
-    }
 
     bytes32 public constant FARMER = keccak256("FARMER");
     bytes32 public constant MEMBER = keccak256("MEMBER");
     bytes32 public constant STAKEHOLDER = keccak256("STAKEHOLDER");
 
-    uint32 constant votingPeriodA = 1 day;
+    uint32 constant votingPeriodA = 1 days;
     uint32 constant votingPeriodB = 2 days;
     uint32 constant votingPeriodC = 3 days;
 
     uint256 public proposalsCount = 0;
     uint256 public eventsCount = 0;
     uint256 public bookingsCount = 0;
+
+    constructor () public ERC20 ("OrganikDAO", "ORGANIK") {
+        _mint(msg.sender, 1000000 * (10 ** uint256(decimals()) ) );
+        // This line will mint 1M coins to the Contract Address.
+    }
 
     // Define all Structs (Keep it simple). Can we map into Moralis DB?
 
@@ -161,7 +161,7 @@ contract OrganikDAO is ReentrancyGuard, AccessControl, ERC20 {
         if (inFavour) proposal.voteInFavor++;
         else proposal.voteAgainst++;
         votes[msg.sender].push(proposalId);
-        _mint(msg.sender, 1 * (10 ** uint256(decimals()) ) )
+        _mint(msg.sender, 1 * (10 ** uint256(decimals()) ) );
     }
 
     // Get ORGANIK Balances
@@ -247,9 +247,9 @@ contract OrganikDAO is ReentrancyGuard, AccessControl, ERC20 {
         proposal.funders.push(Funding(msg.sender, fundAmount, block.timestamp));
         if (proposal.totalFundRaised >= proposal.amount) {
             proposal.isCompleted = true;
-            _mint(proposal.receiverAddress, proposal.totalFundRaised * (10 ** uint256(decimals()) ) )
+            _mint(proposal.receiverAddress, proposal.totalFundRaised * (10 ** uint256(decimals()) ) );
         }
-        _mint(msg.sender, fundAmount * (10 ** uint256(decimals()) ) )
+        _mint(msg.sender, fundAmount * (10 ** uint256(decimals()) ) );
     }
 
     // Release MATIC from Treasury
@@ -299,7 +299,7 @@ contract OrganikDAO is ReentrancyGuard, AccessControl, ERC20 {
             stakeholders[msg.sender] += amount;
         }
         // Mint X ORGANIK Tokens
-        _mint(msg.sender, amount * (10 ** uint256(decimals()) ) )
+        _mint(msg.sender, amount * (10 ** uint256(decimals()) ) );
     }
     
 
